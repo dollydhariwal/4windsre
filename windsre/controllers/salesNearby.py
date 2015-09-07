5094540b72a1e658fdb19dfb326586dd3adbcd4f
dollydhariwal <ddhariwal@gmail.com>
2015-09-07 01:05:07 -0700

    Adding the metadata

--- windsre/controllers/salesNearby.py
+++ windsre/controllers/salesNearby.py
@@ -30,8 +30,10 @@ class FindSalesController(BaseController):
         email: the user email/login
         password: the user password (associated with email)
         """
-        self.input_location = "/home/vjain/4windsre/salesdata"
-        self.output_location = "/home/vjain/4windsre/salesresults"
+        self.input_location = "/home/vipul/4windsre/salesdata"
+        self.output_location = "/home/vipul/4windsre/salesresults"
+        self.input_address_location = "/home/vipul/4windsre/addressdata"
+        self.output_address_location = "/home/vipul/4windsre/addressdb/address.pkl"
         self.r_constant = 0.02526046
         
         
@@ -42,11 +44,62 @@ class FindSalesController(BaseController):
             projectArray.append(filename)
         
         return projectArray
-       
+    
+    
+    def populateAddresses(self):
+        address_dict = {}
+        addict = {}
+        
+        for filename in os.listdir(self.input_address_location):
+            workbook = xlrd.open_workbook("%s/%s" % (self.input_address_location, filename))
+            sheet = workbook.sheet_by_index(0)
+            
+            for row in range(sheet.nrows):
+                #try:
+                address = ""
+                metadata = ""
+                if (row != 0):
+                    for col in range(sheet.ncols):
+                        
+                        try:
+                            if (col==0 or col==1 or col==2):
+                                address = "%s %s" % (address,sheet.cell_value(row,col))
+                            elif (col==3):
+                                metadata = "Beds %s |" % sheet.cell_value(row,col)
+                            elif (col==4):
+                                metadata = "%s Baths %s |" %(metadata, sheet.cell_value(row,col))
+                            elif (col==5):
+                                metadata = "%s Lot Size %s |" %(metadata, sheet.cell_value(row,col))
+                            elif (col==6):
+                                metadata = "%s sqft %s |" %(metadata, sheet.cell_value(row,col))
+                            elif (col==7):
+                                metadata = "%s price %s |" %(metadata, sheet.cell_value(row,col))
+                        except:
+                            pass
+                        
+                    if address:
+                        addict[address] = metadata
+                        address_file = '%s' % (self.output_address_location)
+                        if os.path.exists(address_file):    
+                            file = open(address_file, 'rb')
+                            address_dict = pickle.load(file)
+                            file.close()
+                                
+                        address_dict = dict(address_dict, **addict)
+                        file = open(address_file, 'wb')
+                        pickle.dump(address_dict,file,protocol=2)
+                        file.close()       
         
     def getListProximateAddress(self, start_address=None, radius=1):
+        self.populateAddresses()
+        
+        address_file = '%s' % (self.output_address_location)
+        file = open(address_file, 'rb')
+        address_db = pickle.load(file)
+        file.close()
+        
         address_dict = {}
-        resultDict = []
+        resultDict = {}
                 
         if start_address != None:
             start_location = self.findSales(start_address)
@@ -58,21 +111,22 @@ class FindSalesController(BaseController):
                        
             for row in range(sheet.nrows):
                 #try:
+                address = ""
                 for col in range(sheet.ncols):
                     if (row != 0):
-                        address = sheet.cell_value(row,col)
-                        location = self.findSales(address)
+                        address = "%s %s" % (address, sheet.cell_value(row,col))
+                location = self.findSales(address)
                         
-                        address_file = '%s/%d.pkl' % (self.output_location,int(location[address][0]))
-                        if os.path.exists(address_file):    
-                            file = open(address_file, 'rb')
-                            address_dict = pickle.load(file)
-                            file.close()
+                address_file = '%s/%d.pkl' % (self.output_location,int(location[address][0]))
+                if os.path.exists(address_file):    
+                    file = open(address_file, 'rb')
+                    address_dict = pickle.load(file)
+                    file.close()
                         
-                        address_dict = dict(address_dict, **location)
-                        file = open(address_file, 'wb')
-                        pickle.dump(address_dict,file,protocol=2)
-                        file.close()
+                address_dict = dict(address_dict, **location)
+                file = open(address_file, 'wb')
+                pickle.dump(address_dict,file,protocol=2)
+                file.close()
                 #except:
                 #    pass
                 
@@ -89,7 +143,8 @@ class FindSalesController(BaseController):
             
             long_min = longitude - delta_lon
             long_max = longitude + delta_lon
-                        
+            
+                                 
             if (int(latitude) == int(lat_max)) and (int(lat_min) == int(latitude)): 
                 lat_file = '%s/%d.pkl' % (self.output_location,int(latitude))
                 file = open(lat_file, 'rb')
@@ -98,7 +153,13 @@ class FindSalesController(BaseController):
                
                 for each in address_dict:
                     if address_dict[each][0]>=lat_min and address_dict[each][0]<=lat_max and address_dict[each][1]<=long_max and address_dict[each][1]>=long_min:
-                        resultDict.append(each)
+                        result= {}
+                        try:
+                            result[each] = address_db[each]
+                        except:
+                            result[each] = ""
+                            
+                        resultDict = dict( resultDict, **result)
                     
             if (int(lat_min) != int(lat_max)):
                 try:
@@ -109,7 +170,13 @@ class FindSalesController(BaseController):
                 
                     for each in address_dict:
                         if address_dict[each][0]>=lat_min and address_dict[each][0]<=lat_max and address_dict[each][1]<=long_max and address_dict[each][1]>=long_min:
-                            resultDict.append(each)
+                            result= {}
+                            try:
+                                result[each] = address_db[each]
+                            except:
+                                result[each] = ""
+                                
+                            resultDict = dict( resultDict, **result)
                         
                         
                     lat_file = '%s/%d.pkl' % (self.output_location,int(lat_max))
@@ -119,7 +186,13 @@ class FindSalesController(BaseController):
                 
                     for each in address_dict:
                         if address_dict[each][0]<=lat_min and address_dict[each][0]>=lat_max and address_dict[each][1]<=long_max and address_dict[each][1]>=long_min:
-                            resultDict.append(each)
+                            result= {}
+                            try:
+                                result[each] = address_db[each]
+                            except:
+                                result[each] = ""
+                                
+                            resultDict = dict( resultDict, **result)
                 except:
                     pass
                         
